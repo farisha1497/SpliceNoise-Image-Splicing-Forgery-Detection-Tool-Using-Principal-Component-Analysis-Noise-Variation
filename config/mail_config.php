d7d53715370962155ee8f2f0d5f62d35aac7bcd1
Adam Mirzan bin Ahmad Ridzuan <135194122+adammz02@users.noreply.github.com>
2025-06-18 12:38:58 +0800

    Update mail_config.php

--- config/mail_config.php
+++ config/mail_config.php
@@ -1,8 +1,8 @@
 <?php
 // Direct includes for PHPMailer
-require_once __DIR__ . '/../PHPMailer/src/PHPMailer.php';
-require_once __DIR__ . '/../PHPMailer/src/SMTP.php';
-require_once __DIR__ . '/../PHPMailer/src/Exception.php';
+require_once __DIR__ . '/../vendor/PHPMailer/src/PHPMailer.php';
+require_once __DIR__ . '/../vendor/PHPMailer/src/SMTP.php';
+require_once __DIR__ . '/../vendor/PHPMailer/src/Exception.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
