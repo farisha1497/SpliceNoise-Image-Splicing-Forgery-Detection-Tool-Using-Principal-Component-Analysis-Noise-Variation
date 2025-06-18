a2de2f4d9695a6a26c22403c36d40dcd6be28ab5
Adam Mirzan bin Ahmad Ridzuan <135194122+adammz02@users.noreply.github.com>
2025-06-18 10:51:36 +0800

    Update recaptcha.php

--- config/recaptcha.php
+++ config/recaptcha.php
@@ -1,7 +1,7 @@
 <?php
 // reCAPTCHA v3 Configuration
-define('RECAPTCHA_SITE_KEY', '6Ld35lorAAAAAMwmRK4fl4o3-PUc_-oLehmX5dw4'); 
-define('RECAPTCHA_SECRET_KEY', '6Ld35lorAAAAAKTTmtxICanFaj7mTFT4L9oVECUY'); 
+define('RECAPTCHA_SITE_KEY', '6Lf4p2QrAAAAANHoToFWaxcKvCVmTw73RJOHXLAS'); 
+define('RECAPTCHA_SECRET_KEY', '6Lf4p2QrAAAAAMfFTn8oRZMxy-sdCBpPWyqU6CZj'); 
 define('RECAPTCHA_SCORE_THRESHOLD', 0.5); // Minimum score to consider human (0.0 to 1.0)
 
 // Function to verify reCAPTCHA response
